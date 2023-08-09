495de9e8bac87ac42d5709b48aaa27b9d12e3838
Alaaa1 <alaa0602@hotmail.com>
2023-08-09 20:11:58 +0300

    almost done

--- api-service/src/users/getUser/handler.ts
+++ api-service/src/users/getUser/handler.ts
@@ -1,34 +1,39 @@
 import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
-import { DynamoDBDocumentClient, ScanCommand } from '@aws-sdk/lib-dynamodb';
+import { DynamoDBDocumentClient, QueryCommand } from '@aws-sdk/lib-dynamodb';
 import { middyfy } from "@lib/middleware";
-import { GetObjectCommand, S3Client } from "@aws-sdk/client-s3"
+import cors from '@middy/http-cors';
 
 const client = new DynamoDBClient({});
 const docClient = DynamoDBDocumentClient.from(client);
-const s3Client = new S3Client({});
 
 export default middyfy(async (event) => {
     const { id } = event.pathParameters;
-    const command = new ScanCommand({
-        TableName: process.env.TABLE_NAME!
+    console.log(id);
+    const command = new QueryCommand({
+        TableName: process.env.TABLE_NAME!,
+        KeyConditionExpression:
+            "pk = :id",
+        ExpressionAttributeValues: {
+            ":id": id,
+        },
+        ConsistentRead: true,
     });
 
     const response = await docClient.send(command);
 
-    const s3Command = new GetObjectCommand({
-        Bucket: "api-service-pics-bucket",
-        Key: "f5cecc17-c1cc-4c6c-9e89-f35c95934b00"
-    })
+    // const s3Command = new GetObjectCommand({
+    //     Bucket: "api-service-pics-bucket",
+    //     Key: "private/us-east-1:e6fc63f5-9605-4c41-9b7b-182cbcef0e71/cdac7ac3-125f-45cc-aa98-4e53f713064c"
+    // })
 
-    const s3Object = await (await s3Client.send(s3Command)).Body?.transformToString();
+    // const s3Object = await (await s3Client.send(s3Command)).Body?.transformToString();
 
     return {
         headers: {
-            "Access-Control-Allow-Headers": "Content-Type",
             "Access-Control-Allow-Origin": "*",
             "Access-Control-Allow-Methods": "GET"
         },
         statusCode: 200,
-        body: JSON.stringify({ s3: s3Object, db: response.Items })
+        body: JSON.stringify({ /*s3: s3Object,*/ db: response.Items })
     }
-});
+}).use(cors());
--- api-service/src/users/uploadProfilePicture/handler.ts
+++ api-service/src/users/uploadProfilePicture/handler.ts
@@ -1,21 +1,45 @@
-import { middyfy } from "@lib/middleware";
-import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
+// import { middyfy } from "@lib/middleware";
+// import { S3Client } from "@aws-sdk/client-s3";
+// import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+// import { DynamoDBDocumentClient, UpdateCommand } from "@aws-sdk/lib-dynamodb";
+// import { CognitoIdentityProvider } from "@aws-sdk/client-cognito-identity-provider"
 
-const client = new S3Client({});
-export default middyfy(async (event, context) => {
 
-    const command = new PutObjectCommand({
-        Bucket: "api-service-pics-bucket",
-        Key: context.awsRequestId,
-        // Key: context.identity.cognitoIdentityId,
-        Body: "Hello S3!",
-    });
+// const cognitoClient = new CognitoIdentityProvider({});
+// const client = new DynamoDBClient({});
+// const docClient = DynamoDBDocumentClient.from(client);
+// export default async (event, context) => {
+//     console.log("Alaa stuff", event.requestParameters)
+//     console.log("Alaa stuff", event.requestElements)
+//     console.log("Alaa context", context)
+//     console.log("Alaa event", event.Records[0].s3)
+//     console.log("Alaa identity", event.userIdentity)
+//     const key = event.Records[0].s3.object.key;
+//     const command = new UpdateCommand({
+//         TableName: process.env.TABLE_NAME!,
+//         Key: {
+//             "pk": "alaa",
+//             "sk": "alaa0602@hotmail.com"
+//         },
+//         UpdateExpression: "set pfpKey = :pfpKey",
+//         ExpressionAttributeValues: {
+//             ":pfpKey": key
+//         },
+//         ReturnValues: "ALL_NEW"
+//     });
 
-    const response = await client.send(command);
-    console.log(response);
+//     // const command = new PutObjectCommand({
+//     //     Bucket: "api-service-pics-bucket",
+//     //     Key: context.awsRequestId,
+//     //     // Key: context.identity.cognitoIdentityId,
+//     //     Body: "Hello S3!",
+//     // });
 
-    return {
-        statusCode: 201,
-        body: "Profile Picture Updated Successfully"
-    }
-});
+//     const response = await client.send(command);
+//     // console.log(response);
+
+//     return {
+//         statusCode: 201,
+//         body: "Profile Picture Key Added to DB!"
+//     }
+// };
